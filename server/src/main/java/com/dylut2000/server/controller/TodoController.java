f16722806b737b45360e8ff5d999663b651ece23
Hardy Lutula <hardylutula@gmail.com>
2025-07-22 02:42:32 +0200

    user + todo api-doc added for easy check/test the api, basics controller endpoint added

--- server/src/main/java/com/dylut2000/server/controller/TodoController.java
+++ server/src/main/java/com/dylut2000/server/controller/TodoController.java
@@ -1,7 +1,6 @@
 package com.dylut2000.server.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/todos")
@@ -9,9 +8,32 @@ public class TodoController {
 
     // GET ALL TODOS (ADMIN_ROLE)
 
+    @GetMapping
+    public String getAllTodos() {
+        return "All todos";
+    }
+
+
     // GET ALL TODOS BY USER_ID
+
+    @GetMapping("/{userId}")
+    public String getAllTodosByUserId() {
+        return "All todos by user id";
+    }
+
 
     // UPDATE TODO
 
+    @PutMapping("/{todoId}")
+    public String updateTodo() {
+        return "Todo was updated successfully";
+    }
+
+
     // DELETE TODO
+
+    @DeleteMapping("/{todoId}")
+    public String deleteTodo() {
+        return "Todo was deleted successfully";
+    }
 }
--- server/src/main/java/com/dylut2000/server/controller/UserController.java
+++ server/src/main/java/com/dylut2000/server/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.dylut2000.server.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,18 @@ public class UserController {
 
     // REGISTER USER
 
+    @PostMapping("/register")
+    public String register() {
+        return "User registered successfully!";
+    }
+
+
     // LOGIN USER
+
+    @PostMapping("/login")
+    public String login() {
+        return "User logged in successfully!";
+    }
 
 
 }
